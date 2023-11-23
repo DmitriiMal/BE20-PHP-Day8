f18612a9ac9cefa4d9f2592306822549dc32acab
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-23 13:17:48 +0100

    create all files

---
+++ components/_navbar.php
@@ -0,0 +1,23 @@
+<?php
+$navbar = "
+  <nav>
+    <input id='nav-toggle' type='checkbox' />
+    <div class='logo'>
+      <a href='/PHP-Day-8/index.php'><i class='fa-solid fa-book-open-reader'></i> LibraLink</a>
+    </div>
+    <ul class='links'>
+    <li><a href='/PHP-Day-8/product/product_dashboard.php'>Product</a></li>
+    <li><a href='/PHP-Day-8/user/user_dashboard.php'>Users</a></li>
+    <li><a href='/PHP-Day-8/product/create.php'>Create</a></li>
+    <li><a href='/PHP-Day-8/user/update.php'>Update</a></li>
+    <li><a href='/PHP-Day-8/user/logout.php'>Logout</a></li>
+    <li><a href='/PHP-Day-8/user/login.php'>Login</a></li>
+    <li><a href='/PHP-Day-8/user/register.php'>Register</a></li>
+    </ul>
+    <label for='nav-toggle' class='icon-burger'>
+    <div class='line'></div>
+    <div class='line'></div>
+    <div class='line'></div>
+    </label>
+    </nav>
+    ";
---
+++ components/navbar.php
@@ -0,0 +1,40 @@
+
+
+<?php
+echo "
+<nav class='navbar navbar-expand-lg bg-body-tertiary'>
+  <div class='container-fluid'>
+    <a class='navbar-brand' href='/PHP-Day-8/index.php'>Home</a>
+    <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+      <span class='navbar-toggler-icon'></span>
+    </button>
+    <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+      <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+        <li class='nav-item'>
+          <a class='nav-link active' aria-current='page' href='/PHP-Day-8/product/product_dashboard.php'>Product</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/user/user_dashboard.php'>Users</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/product/create.php'>Create</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/user/update.php'>Update</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/user/logout.php'>Logout</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/user/login.php'>Login</a>
+        </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='/PHP-Day-8/user/register.php'>Register</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+";
